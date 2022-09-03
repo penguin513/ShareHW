49c11d79d2f2f89c3a1701f17617f587c773762a
penguin513 <penguin513@gmail.com>
2022-09-04 02:14:02 +0900

    css/header_fix

--- ShareHW/resources/views/layouts/header.blade.php
+++ ShareHW/resources/views/layouts/header.blade.php
@@ -7,22 +7,7 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Font Awesome -->
-        <script src="https://kit.fontawesome.com/af52375bea.js" crossorigin="anonymous"></script>
-
-
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
-        <!-- Lity  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lity/2.4.1/lity.css" integrity="sha512-NDcw4w5Uk5nra1mdgmYYbghnm2azNRbxeI63fd3Zw72aYzFYdBGgODILLl1tHZezbC8Kep/Ep/civILr5nd1Qw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
 <body>
